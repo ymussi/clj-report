88789b76dc97c0bf7f75c82e0573c36fb29cf1ae
Yuri mussi <yuri.mussi@captalys.com.br>
2019-05-16 19:15:12 -0300

    ajustando report

--- src/report/core.clj
+++ src/report/core.clj
@@ -2,32 +2,6 @@
   (:use clj-pdf.core)
   (:import [java.awt Color]))
 
-;(pdf
-;  [{}
-;   [:list {:roman true}
-;    [:chunk {:style :bold} "Item em negrito"]
-;    "another item"
-;    "yet another item"]
-;   [:phrase "some text"]
-;   [:phrase "some more text"]
-;   [:paragraph "yet more text"]]
-;  "doc.pdf")
-;
-
-;(def stylesheet
-;  {:foo {:color [255 0 0]
-;         :family :helvetica}
-;   :bar {:color [0 0 255]
-;         :family :helvetica}
-;   :baz {:align :right}})
-;
-;(pdf
-;  [{:stylesheet stylesheet}
-;   [:paragraph.foo "item: 0"]
-;   [:paragraph.bar "item: 1"]
-;   [:paragraph.bar.baz "item: 2"]]
-;  "doc.pdf")
-
 (defn radians [degrees] (Math/toRadians degrees))
 
 (defmacro rot [g2d angle & body]
@@ -49,45 +23,29 @@
 
 (pdf
   [
-   {:title         "Test doc"
-    :header        "page header"
-    :subject       "Some subject"
-    :creator       "Jane Doe"
-    :doc-header    ["inspired by" "William Shakespeare"]
+   {:title         "Report"
+    :header        ""
+    :subject       ""
+    :creator       ""
+    :doc-header    [""]
     :right-margin  50
-    :author        "John Doe"
+    :author        ""
     :bottom-margin 10
     :left-margin   10
     :top-margin    20
     :size          "a4"
     :footer        "page"}
 
-   ;[:image
-   ; {:xscale     0.5
-   ;  :yscale     0.8
-   ;  :align      :center
-   ;  :annotation ["FOO" "BAR"]
-   ;  :pad-left   100
-   ;  :pad-right  50}
-   ; (javax.imageio.ImageIO/read (-> "images/logo.jpg" clojure.java.io/resource clojure.java.io/file) )]
-   [:image "images/header_tomatico.PNG"]
+   [:image {:align :center :width 100 :height 75} "images/logo.PNG"]
 
    [:heading
     {:style
-     {:size 15 :align :center}} "Captalys Platform - Resultados da consulta (Idwall)"]
-
-   ;[:chunk {:x 10 :y 10} [:image {:width 16 :height 16} "images/logo.jpg"]]
+     {:size 15 :align :center}} "Captalys Platform - Resultados da consulta"]
 
 
    [:table {:header [{:background-color [100 100 100]} "1.1 - Validações"] :cellSpacing 20}
-    ["Sanções Nações Unidas"]
-
-    ;["foo1" [{:background-color [100 100 100]} "bar1"]]
+    ["Resultados" "teste2"]
     ]
-
-   
-
-
    ]
 
   "report_consultas.pdf")
