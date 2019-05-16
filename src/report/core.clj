35893553883e6c0cbc553da99ec1120af7bc431a
Yuri mussi <yuri.mussi@captalys.com.br>
2019-05-16 19:31:27 -0300

    cleaning code

--- src/report/core.clj
+++ src/report/core.clj
@@ -1,25 +1,5 @@
 (ns report.core
-  (:use clj-pdf.core)
-  (:import [java.awt Color]))
-
-(defn radians [degrees] (Math/toRadians degrees))
-
-(defmacro rot [g2d angle & body]
-  `(do (. ~g2d rotate (radians ~angle))
-       (do ~@body)
-       (. ~g2d rotate (radians (- 0 ~angle)))))
-
-(defmacro trans [g2d dx dy & body]
-  `(do (. ~g2d translate ~dx ~dy)
-       (do ~@body)
-       (. ~g2d translate (- 0 ~dx) (- 0 ~dy))))
-
-(defn draw-tree [g2d length depth]
-  (when (pos? depth)
-    (.drawLine g2d 0 0 length 0)
-    (trans g2d (int length) 0
-           (rot g2d -30 (draw-tree g2d (* length 0.75) (- depth 1)))
-           (rot g2d 30 (draw-tree g2d (* length 0.75) (- depth 1))))))
+  (:use clj-pdf.core))
 
 (pdf
   [
@@ -44,8 +24,7 @@
 
 
    [:table {:header [{:background-color [100 100 100]} "1.1 - Validações"] :cellSpacing 20}
-    ["Resultados" "teste2"]
-    ]
+    ["Resultados" "teste2"]]
    ]
 
   "report_consultas.pdf")
